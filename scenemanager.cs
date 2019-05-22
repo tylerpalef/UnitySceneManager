bfc2e1b07efab40e081520ce704b0a450e09ecd1
“Tyler <tylerpalef@gmail.com>
2019-05-22 14:42:43 -0400

    added the logic for the left arrow, aka going backwards a scene. You are unable to go back a scene when already at the first one

--- scenemanager.cs
+++ scenemanager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement; // Required to be able to deal with scene names/build index
 
 public class scenemanager : MonoBehaviour
@@ -13,6 +14,18 @@ public class scenemanager : MonoBehaviour
 // Update is called every frame 
    void Update()
    {
+   	if(Input.GetKey("left") && sceneNum > 0) // If input of left arrow, and scene number is greather than 0
+   	{
+   		Debug.Log("Left arrow has been pressed"); // Confirming the input to go back a scene
+   		SceneManager.LoadScene((sceneNum - 1), LoadSceneMode.Single); // Subtract 1 from the current 
+   	}
+
+   	if(Input.GetKey("left") && sceneNum <= 0) // If input of left arrow, and scene number is equal or less than 0
+   	{
+   		Debug.Log("Left arrow has been pressed, but the build index cannot go back"); // Confirming the input, however cannot go further back than 0 in the array.
+   	}
+
+
 
    }
  }
