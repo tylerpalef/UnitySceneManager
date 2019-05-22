9acf820c8ca09c9ae368d4465923b0c6ef1cf371
“Tyler <tylerpalef@gmail.com>
2019-05-22 14:30:09 -0400

    adding the public class to the file, as well as void awake and update

---
+++ scenemanager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Required to be able to deal with scene names/build index
+
+public class scenemanager : MonoBehaviour
+{
+// Awake is called sooner than Start.
+   void Awake() 
+   {
+
+   }
+
+// Update is called every frame 
+   void Update()
+   {
+
+   }
+ }
