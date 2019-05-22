1260cb7eb9de8077dbfbea0f6a77bb955529d472
“Tyler <tylerpalef@gmail.com>
2019-05-22 15:01:47 -0400

    refactored the script to use an input manager. Will make changing the keycode for the arrow keys much easier down the line.

--- scenemanager.cs
+++ scenemanager.cs
@@ -4,9 +4,16 @@ using UnityEngine.SceneManagement; // Required to be able to deal with scene nam
 
 public class scenemanager : MonoBehaviour
 {
+	public KeyCode lastSceneKey; // Go back a scene key
+	public KeyCode nextSceneKey; // Go forward a scene key
 // Awake is called sooner than Start.
    void Awake() 
    {
+   	// Setting the values for the last and next scene keys.
+	lastSceneKey = KeyCode.LeftArrow;
+	nextSceneKey = KeyCode.RightArrow;
+
+
    	// sceneNum is the index of the scene from the "Scenes in Build"
    	 int sceneNum = SceneManager.GetActiveScene().buildIndex;
    }
@@ -14,18 +21,30 @@ public class scenemanager : MonoBehaviour
 // Update is called every frame 
    void Update()
    {
-   	if(Input.GetKey("left") && sceneNum > 0) // If input of left arrow, and scene number is greather than 0
+   	// If input of last scene key, and scene number is greather than 0
+   	if(Input.GetKey(lastSceneKey) && sceneNum > 0)
    	{
-   		Debug.Log("Left arrow has been pressed"); // Confirming the input to go back a scene
-   		SceneManager.LoadScene((sceneNum - 1), LoadSceneMode.Single); // Subtract 1 from the current 
+   		// Confirming the input to go back a scene
+   		Debug.Log("Last scene key has been pressed, proceeding to the previous scene"); 
+   		// Add 1 to the current scene build index
+   		SceneManager.LoadScene((sceneNum - 1), LoadSceneMode.Single); 
    	}
 
-   	if(Input.GetKey("left") && sceneNum <= 0) // If input of left arrow, and scene number is equal or less than 0
+   	// If input of last scene key, and scene number is equal or less than 0
+   	if(Input.GetKey(lastSceneKey) && sceneNum <= 0) 
    	{
-   		Debug.Log("Left arrow has been pressed, but the build index cannot go back"); // Confirming the input, however cannot go further back than 0 in the array.
+   		 // Confirming the input, however cannot go further back than 0 in the array.
+   		Debug.Log("Last scene key has been pressed, but the build index cannot go back");
    	}
 
+   	// If input of next scene key and scene number is equal or greather than 0
+   	if(Input.GetKey(nextSceneKey) && sceneNum >= 0) 
+   	{
+   		// Confirming the input to the next scene
+   		Debug.Log("Next scene key has been pressed, proceeding to the next scene");
+   		// Add 1 to the current scene build index
+   		SceneManager.LoadScene((sceneNum + 1), LoadSceneMode.Single); 
+   	}
 
-
-   }
+  }
  }
