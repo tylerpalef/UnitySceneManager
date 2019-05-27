22417be2b0a435e6b07491fc22f89ea22e0c1e99
Tyler Palef <tylerpalef@Sanjeevs-iMac.local>
2019-05-27 17:00:47 -0400

    pushing the end of day code. I have added the animator fading out, but just need to add the fade in

---
+++ Scene Manager Project/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour 
+{
+    public Animator animator; // Animator to control the transition 
+
+    // Use this for initialization
+    void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if(Input.anyKey)
+        {
+            animator.SetTrigger("FadeOut");
+        }
+
+	}
+}
